9dcec52ca47f20422fa80d4a62f721f663b2c1d7
Juan Jose Marinelli <jmarinell@localhost.localdomain>
2016-05-26 11:51:51 -0300

    Colores Pie

--- personal_data/assets/js/pie.js
+++ personal_data/assets/js/pie.js
@@ -5,7 +5,7 @@ var pie = function() {
   var radius = Math.min(width, height) / 2;
 
   var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+      .range(["#FF7900", "#002C77", "#509e3c"]);
 
   var arc = d3.svg.arc()
       .outerRadius(radius - 10)
