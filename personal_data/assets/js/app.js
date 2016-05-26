83bfb24b8e1d01c2e05972f390e7dcbc42599bd5
Juan Jose Marinelli <jmarinell@localhost.localdomain>
2016-05-26 13:05:00 -0300

    asd

--- personal_data/assets/js/app.js
+++ personal_data/assets/js/app.js
@@ -1,13 +1,14 @@
-var json = $.getJSON("assets/js/data.json");
-var installments = 0, no_installments = 0;
-json.responseJSON.forEach(function(d) {
-  if (d.installments){
-    installments++;
-  } else {
-    no_installments++;
-  }
+var json = $.getJSON("assets/js/data.json", function(json) {
+  var installments = 0, no_installments = 0;
+  json.forEach(function(d) {
+    if (d.installments){
+      installments++;
+    } else {
+      no_installments++;
+    }
+  });
+  $("#installments").html(installments);
+  $("#no-installments").html(no_installments);
 });
-$("#installments").html(installments);
-$("#no-installments").html(no_installments);
 histogram();
 pie();
